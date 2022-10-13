11ac16363bcf5168b92391885560af8f61b7cdef
Bravais Martin <91455910+martinbravais@users.noreply.github.com>
2022-10-14 00:04:46 +0200

    Update definir_le_profil.py

--- Projet_Tinder_MARTINKYRIANN-main/definir_le_profil.py
+++ Projet_Tinder_MARTINKYRIANN-main/definir_le_profil.py
@@ -5,7 +5,6 @@ from data import *
 def ajouter_dans_liste_profils (profil):
     texte = "\n" + profil [0] + "," + profil [1] + ","+profil [2] + ","+profil [3] + ","+profil [4]
     with open("C:/Users/marti/OneDrive/Documents/Cours terminal/NSI/Projet_Tinder_MARTINKYRIANN-main/Projet_Tinder_MARTINKYRIANN-main/newdata.txt","a") as fichier:
-        print (texte)
         fichier.write(texte)
         fichier.close()
 
@@ -14,7 +13,7 @@ def creation_de_ton_profil():
     name= input ("Quel est le nom de ton personnage ?  ")
     profil.append (name)
     type = input("Quel est le type ethnique de ton personnage ? \
-                                 réponds bien eurasien, africain, asiatique ou indien\
+                réponds bien eurasien, africain, asiatique ou indien\
                                      ")
     while type not in authorized_types:
                  type = input ("Vous devez absolument choisir parmi ces types: eurasien, africain, asiatique ou indien: \
@@ -22,7 +21,7 @@ def creation_de_ton_profil():
     profil.append(type)
 
     physique = input("Quel est le physique de ton personnage ? \
-                                réponds bien ectomorphe, endomorphe ou mesomorphe\
+                     réponds bien ectomorphe, endomorphe ou mesomorphe\
                                      ")
     while physique not in authorized_physiques:
                  physique = input("Vous devez absolument choisir parmi ces physiques : ectomorphe, endomorphe ou mesomorphe: \
@@ -30,14 +29,14 @@ def creation_de_ton_profil():
 
     profil.append(physique)
     style_musique = input("Quel est le type de musique préféré de ton personnage ? \
-                                 réponds bien jazz, hardrock, rap, reggae ou salsa\
+      réponds bien jazz, hardrock, rap, reggae ou salsa\
                                      ")
     while style_musique not in authorized_musiques :
                  style_musique = input("Vous devez absolument choisir parmi ces musiques : jazz, hardrock, rap, reggae ou salsa: \
                     ")
     profil.append(style_musique)
     caractere = input("Quel est le caractère de ton personnage ? \
-                                 réponds moyenne_ou_typique, reserve, role_modele ou centre_sur_soi)\
+                    réponds moyenne_ou_typique, reserve, role_modele ou centre_sur_soi)\
                                      ")
     while caractere not in authorized_caracteres :
           caractere = input("Vous devez absolument choisir parmi ces musiques : moyenne_ou_typique, reserve, role_modele ou centre_sur_soi: \
@@ -47,4 +46,5 @@ def creation_de_ton_profil():
     ajouter_dans_liste_profils (profil)
 
 creation_de_ton_profil()
+
 
